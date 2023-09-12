f5d11bd03acbdb0bd22b61aa2e4c773984ec4f4d
meishakrsc <meishakrsc22@gmail.com>
2023-09-12 14:48:22 +0700

    Saya menambahkan tugas

--- tugas1laravel/resources/views/template/perpus/anggota.blade.php
+++ tugas1laravel/resources/views/template/perpus/anggota.blade.php
@@ -30,7 +30,8 @@
                   </div>
                 <!-- /.card-body -->
                 <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" name="submit" value="Save" class="btn btn-primary">Submit</button>
+                <button type="reset" class="btn btn-primary">Reset</button>
                 </div>
               </form>
             </div>
